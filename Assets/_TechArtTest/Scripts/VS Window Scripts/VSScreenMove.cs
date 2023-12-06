5a9d1f27908c411da1c0d7aaccffb491473ec353
Omri Ovadia <omriovadia8@gmail.com>
2023-12-06 18:19:39 +0200

    fixed lighting

--- Assets/_TechArtTest/Scripts/VS Window Scripts/VSScreenMove.cs
+++ Assets/_TechArtTest/Scripts/VS Window Scripts/VSScreenMove.cs
@@ -11,8 +11,8 @@ public class VSScreenMove : MonoBehaviour
     [Header("Animation Settings")]
     [SerializeField] private float moveDistance = 700f;
     [SerializeField] private float overshootDistance = 100f;
-    [SerializeField] private float durationFast = 0.5f;
-    [SerializeField] private float durationSlow = 1.0f;
+    [SerializeField] private float easeInDuration = 0.5f;
+    [SerializeField] private float easeOutDuration = 1.0f;
 
     private Vector2 originalPosition;
 
@@ -26,11 +26,11 @@ public class VSScreenMove : MonoBehaviour
     {
         Sequence sequence = DOTween.Sequence();
 
-        sequence.Append(rectTransform.DOAnchorPosX(GetTargetPosition(-moveDistance - overshootDistance), durationFast).SetEase(Ease.OutQuad));
-        sequence.Join(fillImage.DOFillAmount(1, durationFast));
-        sequence.Append(rectTransform.DOAnchorPosX(GetTargetPosition(-moveDistance), durationSlow).SetEase(Ease.OutCubic));
+        sequence.Append(rectTransform.DOAnchorPosX(GetTargetPosition(-moveDistance - overshootDistance), easeInDuration).SetEase(Ease.OutQuad));
+        sequence.Join(fillImage.DOFillAmount(1, easeInDuration));
+        sequence.Append(rectTransform.DOAnchorPosX(GetTargetPosition(-moveDistance), easeOutDuration).SetEase(Ease.OutCubic));
         sequence.AppendCallback(() => fillImage.fillOrigin = 0);
-        sequence.Append(fillImage.DOFillAmount(0, durationSlow));
+        sequence.Append(fillImage.DOFillAmount(0, easeOutDuration));
     }
 
     private float GetTargetPosition(float offset)
